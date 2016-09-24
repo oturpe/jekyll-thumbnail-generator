6484c21531b68794763f6df42b5669ee558cb3af
Otto Urpelainen <oturpe@iki.fi>
2016-09-24 17:50:52 +0300

    Whitespace cleanup

--- thumbnail_generator.rb
+++ thumbnail_generator.rb
@@ -10,24 +10,23 @@ module Jekyll
     LargeDimension = 1e4
 
     def generate(site)
-    	# TODO
-    	site.posts.docs.each do |post|
-    	    gallery = post.data["gallery"]
-    	    next if not gallery
+        # TODO
+        site.posts.docs.each do |post|
+            gallery = post.data["gallery"]
+            next if not gallery
 
-    	    directory = "assets/posts/#{post.data['slug']}";
+            directory = "assets/posts/#{post.data['slug']}";
 
             thumbnails_dir = "#{directory}/thumbnails"
             Dir.mkdir thumbnails_dir if not (File.exists? thumbnails_dir)
 
-    	    gallery.each do |item|
-    	        image = Image.read("#{directory}/#{item['file']}")[0]
-    	        image.resize_to_fit(LargeDimension, 240)
+            gallery.each do |item|
+                image = Image.read("#{directory}/#{item['file']}")[0]
+                image.resize_to_fit(LargeDimension, 240)
 
                 thumbnail_file = "#{thumbnails_dir}/#{item['file']}"
                 image.write(thumbnail_file) if not File.exists? thumbnail_file
     	    end
-        print "\n"
         end
     end
 
