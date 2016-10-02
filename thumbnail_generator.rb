62fc44a8a904bcb3619e5f2c567148055bae46d9
Otto Urpelainen <oturpe@iki.fi>
2016-10-02 17:24:26 +0300

    Small refactoring: make the thumbnails directory name a constant.
    This is just for cleaner reading.

--- thumbnail_generator.rb
+++ thumbnail_generator.rb
@@ -9,19 +9,25 @@ module Jekyll
     # Value to use as an image dimension when no limit is desired
     LargeDimension = 1e4
 
+    # Directory names
+    Thumbnails_dir = "thumbnails"
+
     def generate(site)
         # TODO
         site.posts.docs.each do |post|
             gallery = post.data["gallery"]
             next if not gallery
 
-            directory = "assets/posts/#{post.data['slug']}";
+            asset_dir = "assets/posts/#{post.data['slug']}";
+            if not File.exists? asset_dir
+                raise "Asset directory '#{asset_dir}' not found."
+            end
 
-            thumbnails_dir = "#{directory}/thumbnails"
+            thumbnails_dir = "#{asset_dir}/#{Thumbnails_dir}"
             Dir.mkdir thumbnails_dir if not (File.exists? thumbnails_dir)
 
             gallery.each do |item|
-                image = Image.read("#{directory}/#{item['file']}")[0]
+                image = Image.read("#{asset_dir}/#{item['file']}")[0]
                 image.resize_to_fit!(LargeDimension, 120)
 
                 thumbnail_file = "#{thumbnails_dir}/#{item['file']}"
